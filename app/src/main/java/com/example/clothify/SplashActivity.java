357979f3409e56f59dc09d95c9aa9ccce0ef3cc5
Dhruv <druvpatel12112001@gmail.com>
2023-01-19 11:22:02 +0530

    Login activity and splash activity xml done

--- app/src/main/java/com/example/clothify/SplashActivity.java
+++ app/src/main/java/com/example/clothify/SplashActivity.java
@@ -1,31 +1,31 @@
 package com.example.clothify;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
+import android.os.Bundle;
 import android.view.WindowManager;
-
-import pl.droidsonroids.gif.GifImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Intent i=new Intent(getApplicationContext(),
+                        LoginActivity.class);
 
-                Intent i = new Intent(SplashActivity.this,LoginActivity.class);
                 startActivity(i);
+
                 finish();
             }
-        },3000);
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
